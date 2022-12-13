03e393703d0ce64c99c45dce34385e46f0619970
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2022-12-13 17:58:06 +0200

    Game win condition

--- logic/game.py
+++ logic/game.py
@@ -5,6 +5,18 @@ import json
 from pgnet import BaseGame, Packet, Response, STATUS_UNEXPECTED
 
 
+WINNING_LINES = [
+    (0, 1, 2),
+    (3, 4, 5),
+    (6, 7, 8),
+    (0, 3, 6),
+    (1, 4, 7),
+    (2, 5, 8),
+    (0, 4, 8),
+    (2, 4, 6),
+]
+
+
 def get_blank_save_string() -> dict:
     return json.dumps(dict(
         board=[""] * 9,
@@ -15,8 +27,6 @@ def get_blank_save_string() -> dict:
 
 class Game(BaseGame):
 
-    persistent = True
-
     def __init__(self, *args, save_string: Optional[str] = None, **kwargs):
         super().__init__(*args, **kwargs)
         save_string = save_string or get_blank_save_string()
@@ -24,11 +34,17 @@ class Game(BaseGame):
         self.board: list[str] = data["board"]
         self.players: list[str] = data["players"]
         self.x_turn: bool = data["x_turn"]
+        self.outcome: str = "In progress."
+        self.in_progress: bool = False
         self.commands = dict(
             get_state_hash=self.get_state_hash,
             get_full_data=self.get_full_data,
             play_square=self.play_square,
         )
+
+    @property
+    def persistent(self):
+        return self.in_progress and any(self.board)
 
     def get_save_string(self) -> str:
         data = dict(
@@ -41,6 +57,8 @@ class Game(BaseGame):
     def user_joined(self, player: str):
         if player not in self.players:
             self.players.append(player)
+        if len(self.players) >= 2:
+            self.in_progress = True
 
     def user_left(self, player: str):
         if player in self.players[2:]:
@@ -54,7 +72,7 @@ class Game(BaseGame):
 
     # Logic
     @property
-    def state_hash(self) -> str:
+    def _state_hash(self) -> str:
         data = [
             str(self.board),
             str(self.players),
@@ -63,45 +81,64 @@ class Game(BaseGame):
         final = hash(tuple(data))
         return final
 
-    def _get_turn_username(self) -> str:
-        if len(self.players) < 2:
+    @property
+    def _current_username(self) -> str:
+        if not self.in_progress:
             return ""
         return self.players[int(self.x_turn)]
 
+    def _check_progress(self):
+        for a, b, c in WINNING_LINES:
+            mark = self.board[a]
+            if mark and mark == self.board[b] == self.board[c]:
+                self.in_progress = False
+                self.outcome = f"{self._mark_to_username(mark)} playing as {mark} wins!"
+                return
+        if all(self.board):
+            self.in_progress = False
+            self.outcome = "Draw."
+
+    def _mark_to_username(self, mark: str):
+        assert mark
+        return self.players[0 if mark == "O" else 1]
+
+    def _username_to_mark(self, username: str):
+        assert username in self.players[:2]
+        return "O" if username == self.players[0] else "X"
+
     # Commands
     def get_state_hash(self, packet: Packet) -> Response:
-        r = Response("Current hash", dict(state_hash=self.state_hash))
+        r = Response("Current hash", dict(state_hash=self._state_hash))
         return r
 
     def get_full_data(self, packet: Packet) -> Response:
         payload = dict(
-            state_hash=self.state_hash,
+            state_hash=self._state_hash,
             players=self.players,
             board=self.board,
-            your_turn=packet.username == self._get_turn_username(),
+            your_turn=packet.username == self._current_username,
             info=self._get_user_info(packet.username),
         )
         return Response("Full data", payload)
 
     def play_square(self, packet: Packet) -> Response:
-        user = self._get_turn_username()
-        if packet.username != user:
+        username = self._current_username
+        if packet.username != username:
             return Response("Not your turn.", status=STATUS_UNEXPECTED)
         square = int(packet.payload["square"])
         if self.board[square]:
             return Response("Square is already marked.", status=STATUS_UNEXPECTED)
-        mark = "X" if self.x_turn else "O"
-        self.board[square] = mark
+        self.board[square] = self._username_to_mark(username)
         self.x_turn = not self.x_turn
+        self._check_progress()
         return Response("Marked square.")
 
     def _get_user_info(self, username: str) -> str:
-        current_username = self._get_turn_username()
+        if not self.in_progress:
+            return self.outcome
+        current_username = self._current_username
+        if username not in self.players[:2]:
+            mark = self._username_to_mark(current_username)
+            return f"{self.outcome}\nSpectating {current_username}'s turn as {mark}"
         turn = "Your turn" if username == current_username else "Awaiting turn"
-        if username == self.players[0]:
-            return f"{turn}, playing as: O"
-        elif username == self.players[1]:
-            return f"{turn}, playing as: X"
-        else:
-            mark = "X" if self.x_turn else "O"
-            return f"Spectating {current_username}'s turn: {mark}"
+        return f"{turn}, playing as: {self._username_to_mark(username)}"
