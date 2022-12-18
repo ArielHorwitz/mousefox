0868f66199c69cb72b7a4362b92b08eb1db84051
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2022-12-18 23:23:57 +0200

    Use client heartbeat for updates

--- logic/client.py
+++ logic/client.py
@@ -1,7 +1,6 @@
 """Logic client."""
 
 from typing import Optional, Callable
-import asyncio
 from loguru import logger
 import pgnet.client
 import pgnet.localhost
@@ -9,22 +8,20 @@ import logic.game
 import util
 
 
-HEARTBEAT_INTERVAL = 0.5
-
-
 class Client(pgnet.client.BaseClient):
-    """Subclass of pgnet Client for this game."""
+    """Networking client."""
 
     def __init__(self, *args, on_game_state: Optional[Callable] = None, **kwargs):
         super().__init__(*args, **kwargs)
-        self.game_state: dict = {"state_hash": ""}
+        self.game_state: dict = {"state_hash": None}
         self.on_game_state = on_game_state
 
-    def queue_update(self, *args, **kwargs):
-        self.send(*args, **kwargs)
-        self.check_update(do_next=False)
+    def heartbeat(self):
+        """Override base method to automatically update game state."""
+        self.check_update()
 
     def check_update(self, *, do_next: bool = True):
+        """Check for and apply updates to game state."""
         state_hash = self.game_state.get("state_hash")
         self.send(
             pgnet.Packet("check_update", dict(state_hash=state_hash)),
@@ -43,17 +40,6 @@ class Client(pgnet.client.BaseClient):
             logger.warning(f"Missing state hash: {self.game_state=}")
         if self.on_game_state:
             self.on_game_state(self.game_state)
-
-    async def async_connect(self):
-        heartbeat = asyncio.create_task(self._heartbeat())
-        await super().async_connect()
-        heartbeat.cancel()
-
-    async def _heartbeat(self):
-        while True:
-            await asyncio.sleep(HEARTBEAT_INTERVAL)
-            if self.connected and self.game:
-                self.check_update()
 
 
 class LocalhostClient(pgnet.localhost.LocalhostClientMixin, Client):
